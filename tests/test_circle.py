e4a12718fa47d590c197928f967b11f6815f1c54
fasthill <kangeun@naver.com>
2024-05-09 16:18:33 +0900

    finish TestClass

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 import source.shapes as shapes
@@ -6,15 +8,17 @@ class TestCircle:
 
     def setup_method(self, method):
         print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
 
     def teardown_method(self, method):
         print(f"Tearing down {method}")
-
-    def test_one(self):
-        assert True
-
-    def test(self):
-        pass
+        del self.circle
 
 
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+    def test_perimeter(self):
+        result = self.circle.perimeter()
+        expected = 2 * math.pi * self.circle.radius
+        assert result == expected
 

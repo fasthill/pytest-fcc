27f9c0abdeb3f9d9631ac187868da1060e9897a7
fasthill <kangeun@naver.com>
2024-05-10 10:27:58 +0900

    parameterize-2

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -9,9 +9,11 @@ def test_add():
     result = my_functions.add(1, 4)
     assert result == 5
 
+
 def test_add_strings():
     result = my_functions.add("i like ", "burgers")
     assert result == "i like burgers"
+
 
 def test_divide():
     result = my_functions.divide(10, 5)
@@ -22,15 +24,18 @@ def test_divide_by_zero():
     with pytest.raises(ValueError):
         my_functions.divide(10, 0)
 
+
 @pytest.mark.slow
 def test_very_slow():
     time.sleep(5)
     result = my_functions.divide(10, 5)
     assert result == 2
 
+
 @pytest.mark.skip(reason="This feature is currently broken")
 def test_add():
     assert my_functions.add(1, 2) == 3
+
 
 @pytest.mark.xfail(reason="We know we cannot divide by zero")
 def test_divide_by_zero_broken():

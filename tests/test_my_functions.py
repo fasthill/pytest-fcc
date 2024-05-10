c982d63c09467337a3d2f4e9b67e429ecc7c9011
fasthill <kangeun@naver.com>
2024-05-10 10:26:26 +0900

    finish TestClass

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,5 +1,7 @@
 import pytest
 
+import time
+
 import source.my_functions as my_functions
 
 
@@ -19,3 +21,17 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         my_functions.divide(10, 0)
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_functions.add(1, 2) == 3
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+def test_divide_by_zero_broken():
+    assert my_functions.divide(4, 0) == 4

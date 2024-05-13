1c18ed8879d2b5f6b87f7cc98dd63be170b424d6
fasthill <kangeun@naver.com>
2024-05-13 13:42:02 +0900

    test square perimetrize

--- source/shapes.py
+++ source/shapes.py
@@ -37,4 +37,7 @@ class Rectangle(Shape):
     def perimeter(self):
         return (2 * self.length) + (self.width * 2)
 
-    
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
